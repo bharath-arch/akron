1f4fe20b72c9a3036a5a34c1c6a121da77313b8c
Bharath <56076642+Bharath09876@users.noreply.github.com>
2024-04-04 07:36:54 -0700

    v4

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -61,79 +61,85 @@ function Layout() {
             </li>
           </ul>
         </div>
-
-        <div className="flex gap-4 items-center ">
-          <p className="hidden md:items-center md:p-2 md:text-xl">
-            <Link to={"/sign_in"}>Sign In</Link>
-          </p>
-          <Link to={"/Login"}>
-            {" "}
-            <button
-              className={`md:p-3 hidden md:text-xl md:bg-blue-800 md:text-white md:rounded-lg`}
-            >
-              Get Started
+        <div className="">
+          <div className="hidden  md:flex md:gap-4 md:items-center ">
+            <p className=" md:items-center md:p-2 md:text-xl">
+              <Link to={"/sign_in"}>Sign In</Link>
+            </p>
+            <Link to={"/Login"}>
+              {" "}
+              <button
+                className={`md:p-3  md:text-xl md:bg-blue-800 md:text-white md:rounded-lg`}
+              >
+                Get Started
+              </button>
+            </Link>
+          </div>
+          <div>
+            <button onClick={navtoggle} className="md:hidden">
+              {isOpen ? <RxHamburgerMenu /> : <RxHamburgerMenu />}
             </button>
-          </Link>
-          <button onClick={navtoggle} className="md:hidden">
-            {isOpen ? <RxHamburgerMenu /> : <RxHamburgerMenu />}
-          </button>
-          
-          {isOpen && (
-            <div className="relative ">
-              <ul className=" absolute flex flex-col items-center text-[12px] top-5  right-4 font-semibold  bg-white md:hidden">
-                <li
-                  onClick={() => setSelectLink("Home")}
-                  className={`${
-                    selectLink === "Home" ? "text-blue-900 p-2" : "p-2"
-                  }`}
-                >
-                  <Link to={""}>Home</Link>
-                </li>
-                <hr className="" />
-                <li
-                  onClick={() => setSelectLink("startup")}
-                  className={`${
-                    selectLink === "startup" ? "text-blue-900 p-2" : "p-2"
-                  }`}
-                >
-                  <Link to={"/startup"}>Startup</Link>
-                </li>
-                <hr className="" />
-                <li
-                  onClick={() => setSelectLink("academy")}
-                  className={`${
-                    selectLink === "academy" ? "text-blue-900 p-2" : "p-2"
-                  }`}
-                >
-                  <Link to={"/academy"}>Academy</Link>
-                </li>
-                <hr className="" />
-                <li
-                  onClick={() => setSelectLink("partnership")}
-                  className={`${
-                    selectLink === "partnership" ? "text-blue-900 p-2" : "p-2"
-                  }`}
-                >
-                  <Link to={"/partnership"}>Partnership</Link>
-                </li>
-                <hr className="" />
-                <li
-                  onClick={() => setSelectLink("Aboutus")}
-                  className={`${
-                    selectLink === "Aboutus" ? "text-blue-900 p-2" : "p-2"
-                  }`}
-                >
-                  <Link to={"/Aboutus"}>About Us</Link>
-                </li><hr className="" />
-                <li onClick={() => setSelectLink("sign_in")}
-                  className={`${
-                    selectLink === "sign_in" ? "text-blue-900 p-2" : "p-2"
-                  }`}>
-                  <Link to={"/sign_in"}>Sign In</Link>
-                </li>
-              </ul>
-            </div>
-          )}
+
+            {isOpen && (
+              <div className="relative ">
+                <ul className=" absolute flex flex-col items-center text-[12px] top-5  right-4 font-semibold  bg-white md:hidden">
+                  <li
+                    onClick={() => setSelectLink("Home")}
+                    className={`${
+                      selectLink === "Home" ? "text-blue-900 p-2" : "p-2"
+                    }`}
+                  >
+                    <Link to={""}>Home</Link>
+                  </li>
+                  <hr className="" />
+                  <li
+                    onClick={() => setSelectLink("startup")}
+                    className={`${
+                      selectLink === "startup" ? "text-blue-900 p-2" : "p-2"
+                    }`}
+                  >
+                    <Link to={"/startup"}>Startup</Link>
+                  </li>
+                  <hr className="" />
+                  <li
+                    onClick={() => setSelectLink("academy")}
+                    className={`${
+                      selectLink === "academy" ? "text-blue-900 p-2" : "p-2"
+                    }`}
+                  >
+                    <Link to={"/academy"}>Academy</Link>
+                  </li>
+                  <hr className="" />
+                  <li
+                    onClick={() => setSelectLink("partnership")}
+                    className={`${
+                      selectLink === "partnership" ? "text-blue-900 p-2" : "p-2"
+                    }`}
+                  >
+                    <Link to={"/partnership"}>Partnership</Link>
+                  </li>
+                  <hr className="" />
+                  <li
+                    onClick={() => setSelectLink("Aboutus")}
+                    className={`${
+                      selectLink === "Aboutus" ? "text-blue-900 p-2" : "p-2"
+                    }`}
+                  >
+                    <Link to={"/Aboutus"}>About Us</Link>
+                  </li>
+                  <hr className="" />
+                  <li
+                    onClick={() => setSelectLink("sign_in")}
+                    className={`${
+                      selectLink === "sign_in" ? "text-blue-900 p-2" : "p-2"
+                    }`}
+                  >
+                    <Link to={"/sign_in"}>Sign In</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       </div>
       <div className="flex flex-col min-h-screen">
