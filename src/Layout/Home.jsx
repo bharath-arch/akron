76ad22ace179c86442880a0592bdb27ea8db3a8c
bharathkumar <bharathkumar100q@gmail.com>
2024-08-04 22:11:22 +0530

    done

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -7,23 +7,23 @@ function Home() {
     <div className="w-screen h-screen flex items-center justify-center bg-gray-100 p-4 md:p-6 lg:p-8">
       <div className="max-w-4xl w-full bg-white shadow-lg rounded-lg flex flex-col md:flex-row md:items-center p-6 md:p-12">
         <div className="flex-1 mt-6 md:mt-0">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-center md:text-left mb-4 md:mb-6">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-semibold text-center mb-4 md:mb-6">
             Grow Your Wealth with Higher Returns
           </h1>
-          <p className="text-base md:text-lg lg:text-xl text-center md:text-left text-gray-600 mb-6">
+          <p className="text-base md:text-lg lg:text-xl xl:text-2xl text-center text-gray-600 mb-6 md:mb-8">
             Your platform for exclusive investment opportunities
           </p>
 
-          <div className="flex justify-center md:justify-start mb-6">
+          <div className="flex justify-center  mb-6">
             <Link to="/Login">
-              <button className="px-6 py-3 bg-blue-800 hover:bg-blue-700 text-white rounded-lg text-lg md:text-xl transition-transform duration-300 ease-in-out transform hover:scale-105">
+              <button className="px-4 py-2 md:px-6 md:py-3 bg-blue-800 hover:bg-blue-700 text-white rounded-lg text-base md:text-lg lg:text-xl transition-transform duration-300 ease-in-out transform hover:scale-105">
                 Get Started
               </button>
             </Link>
           </div>
         </div>
 
-       
+
       </div>
     </div>
   );
